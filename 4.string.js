9d561bbf48441b4ec01b6ec0b912bc6d429164e2
JuYeon <kirimaki1@nate.com>
2023-01-16 00:14:33 +0900

    variable study

---
+++ 4.string.js
@@ -0,0 +1,10 @@
+let string = "안녕하세요";
+let number = 44;
+string = "안녕하세요" 
++ number;
+
+
+// 템플릿 리터럴 (Template Literal) 백틱을 사용해서 편하게 쓸수있음!!
+string = `안녕하세요? ${number}`;
+console.log(string);
+console.log()
