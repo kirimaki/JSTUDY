df315fcde69f4fc4c622798a961937369206bbeb
kirimaki <kirimaki1@nate.com>
2023-02-16 23:59:33 +0900

    study

---
+++ 1.iterable.js
@@ -0,0 +1,13 @@
+const array = [1, 2, 3];
+
+for(const item of array) {
+    console.log(item);
+}
+
+const obj = {0: 1, 1: 2 };
+
+// for(const item of obj) {
+//     console.log(item);
+// }
+
+console.log(array.values());
